0be31191e1e808e6c7c38edc088eed63b33fd42c
Jon Hoffman <166093179+jomhoff@users.noreply.github.com>
2024-12-17 10:54:08 -0500

    Create compleasm.sh

---
+++ compleasm.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#SBATCH --job-name compleasm
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=8
+#SBATCH --mem=20gb
+#SBATCH --time=5:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jhoffman1@amnh.org
+
+source ~/.bash_profile
+conda activate compleasm
+
+GENOME="/path/to/genome.fasta"
+OUT="/path/to/outdirectory"
+LINEAGE="busco_lineage" #i.e. sauropsida
+
+compleasm run -a $GENOME -o $OUT -t $SLURM_NTASKS_PER_NODE -l $LINEAGE
+
